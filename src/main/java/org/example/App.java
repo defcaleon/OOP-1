99f6935ba2736d775d258500502844e460d57087
Nikita Parmon <nikitka.parmon@mail.ru>
2021-02-14 15:27:23 +0300

    Draw simple figures

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,16 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,22 +24,47 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+
+        stage.setTitle("Drawing Operations Test");
         stage.setScene(scene);
+
+        Group root = new Group();
+
+        Canvas canvas=new Canvas(640,480);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        root.getChildren().add(canvas);
+        drawing(gc);
+
+        stage.setScene(new Scene(root));
         stage.show();
+
+
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
 
     public static void main(String[] args) {
         launch();
+    }
+
+
+    private void drawing(GraphicsContext gc){
+
+        gc.setStroke(Color.GOLD);
+        gc.setFill(Color.BLUE);
+        gc.setLineWidth(5);
+
+        gc.setLineWidth(5);
+        gc.strokeLine(50,200,150,200);
+
+
+        gc.strokeRect(50, 50, 100, 100);
+
+        gc.strokeArc(200,200,30,30,45,360,ArcType.OPEN);
+
+
+        gc.strokePolygon(new double[]{50,100,50,100,50},new double[]{220,220,250,250,240},4);
+        gc.strokePolyline(new double[]{300,400,400,500},new double[]{300,300,400,400},4);
     }
 
 }
